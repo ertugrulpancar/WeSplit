85e816153b4443bead9092b172614b812b50c33f
Ertuğrul Pancar <21220030023@mersin.edu.tr>
2024-11-21 15:11:20 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ertuğrul Pancar on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
